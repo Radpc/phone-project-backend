a003e002b30a47150f1917f99bf2b4008d23ccf0
Matheus Marques <mgm3@icomp.ufam.edu.br>
2025-04-25 10:48:27 -0400

    feat: add swagger document generation

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Device API')
+    .setDescription('An API made for an entrance project')
+    .setVersion('1.0')
+    .addTag('device')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
 
   // Pipes
   app.useGlobalPipes(new ValidationPipe());
