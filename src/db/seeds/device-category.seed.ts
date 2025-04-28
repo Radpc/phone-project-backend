c37bef390e82fedfc1ba66401621c4b865ba9927
Matheus Marques <mgm3@icomp.ufam.edu.br>
2025-04-28 13:42:57 -0400

    feat: add more seeds

---
+++ src/db/seeds/device-category.seed.ts
@@ -0,0 +1,7 @@
+import { Prisma } from '../../../generated/prisma';
+
+export const deviceCategorySeed: Prisma.DeviceCategoryCreateManyInput[] = [
+  { name: 'Category A' },
+  { name: 'Category B' },
+  { name: 'Category C' },
+];
--- src/db/seeds/device.seed.ts
+++ src/db/seeds/device.seed.ts
@@ -1,3 +1,7 @@
 import { Prisma } from '../../../generated/prisma';
 
-export const deviceSeed: Prisma.DeviceCreateManyInput[] = [];
+export const deviceSeed: Prisma.DeviceCreateManyInput[] = [
+  { color: 'red', deviceCategoryId: 1, partNumber: 501 },
+  { color: 'green', deviceCategoryId: 1, partNumber: 502 },
+  { color: 'blue', deviceCategoryId: 1, partNumber: 503 },
+];
